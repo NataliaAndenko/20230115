93975dba1ddeadb755af9755ec234dc63cebfd74
Natalia <zhuravlevanyu@mail.ru>
2023-01-15 15:48:33 +0300

    учусь

---
+++ 003/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. 
+Если число 2 не кратно числу 1, то программа выводит остаток от деление.
+34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно
+*/
+int InputInt(string massage)
+{
+    Console.WriteLine(massage);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int a = InputInt("Введите 1 число");
+int b = InputInt("Введите 2 число");
+if (a % b == 0)
+Console.WriteLine("Да, кратно");
+else Console.WriteLine("Нет, не кратно, остаток " + a % b);
---
+++ 004/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход число и проверяет, 
+кратно ли оно одновременно 7 и 23.
+14 -> нет 
+46 -> нет 
+161 -> да
+*/
+int InputInt(string massage)
+{
+    Console.WriteLine(massage);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int a = InputInt("Введите 1 число");
+int b = 7;
+int c = 23;
+
+if (a % b == 0 && a % c == 0)
+    Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
