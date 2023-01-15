35d73a674cad745ac577ceeb7125d9c363ccf2dd
Natalia <zhuravlevanyu@mail.ru>
2023-01-15 14:48:12 +0300

    first commit

---
+++ 001/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 1. Напишите программу, которая принимает на вход трёхзначное число и
+ на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+int InputInt(string massage)
+{
+    Console.WriteLine(massage);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int a = InputInt("Введите трехзначное число");
+if (a > 99 && a < 1000)
+{
+    int result = a % 10;
+    Console.WriteLine("Последняя цифра числа равна " + result);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
